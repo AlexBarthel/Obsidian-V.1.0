a78dd8194558dc29e236e3dc50facd038b021d3e
AlexBarthel <98185865+AlexBarthel@users.noreply.github.com>
2022-01-21 18:50:28 -0600

    Add files via upload

---
+++ Modules/getbuildinfo.py
@@ -0,0 +1,26 @@
+GITVERSION = 0.1;
+GITTAG     = 'default';
+buildinfo = {
+	'build-month': 'Nov',
+	'build-day': 12,
+	'build-year': 2021,
+};
+
+
+def Obs_GetBuildInfo():
+    gitid = _Obs_gitidentifier();
+    revision = _Obs_gitversion();
+    print(
+        f'Obsidian+ {revision} ({gitid} {buildinfo["build-month"]} {buildinfo["build-day"]} {buildinfo["build-year"]})'
+    )
+
+
+def _Obs_gitversion():
+	return GITVERSION;
+
+
+def _Obs_gitidentifier():
+	gitid = 'default';
+	if (GITTAG != 'default'):
+		gitid = GITTAG;
+	return gitid;
